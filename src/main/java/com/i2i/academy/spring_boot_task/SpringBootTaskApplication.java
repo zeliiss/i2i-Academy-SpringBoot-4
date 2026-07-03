16b6131b8277f7a27e2068363a6cd3f680e12cdd
zeliiss <zelihaezer@icloud.com>
2026-07-03 16:20:19 +0300

    Initial commit: Set up basic Spring Boot project structure

---
+++ src/main/java/com/i2i/academy/spring_boot_task/SpringBootTaskApplication.java
@@ -0,0 +1,13 @@
+package com.i2i.academy.spring_boot_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskApplication.class, args);
+	}
+
+}
